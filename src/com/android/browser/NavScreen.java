184aa342c10acd14fc03e0e2cea8f0f2040828d5
Lijunjie <lijunjieone@gmail.com>
2015-09-02 16:03:23 +0800

    调整调用代码
    Change-Id: If0036afa4a1966c2b0e98bbf888aeb891a467b0c

--- src/com/android/browser/NavScreen.java
+++ src/com/android/browser/NavScreen.java
@@ -167,52 +167,38 @@ public class NavScreen extends RelativeLayout
         });
     }
 
+    private void showQuickDial(View v) {
+            final QuickDialWindow q=new QuickDialWindow(mActivity);
+            q.getQuickDialDragLayer().setQuickDialOpenUrlListener(new QuickDialOpenUrlListener() {
+                
+                @Override
+                public void openUrl(String url) {
+                    Intent intent = null;
+                    intent = new Intent(Intent.ACTION_VIEW);
+                    intent.setData(Uri.parse(url));
+                    intent.putExtra(MxActionDefine.EXTRA_APPLICATION_ID,
+                            MxActionDefine.SOURCE_LOCAL_APPID);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                    intent.setClassName(getContext(), BrowserActivity.class.getName());
+                    getContext().startActivity(intent);
+                    q.dimiss();
+                    
+                }
+            });
+            q.show(v);
+
+    }
     @Override
     public void onClick(View v) {
         if (mBookmarks == v) {
             mUiController.bookmarksOrHistoryPicker(ComboViews.Bookmarks);
         } else if (mNewIncognitoTab == v || mNewTab == v) {
             // openNewTab(mNewIncognitoTab == v);
-            final QuickDialWindow q=new QuickDialWindow(mActivity);
-            q.getQuickDialDragLayer().setQuickDialOpenUrlListener(new QuickDialOpenUrlListener() {
-                
-                @Override
-                public void openUrl(String url) {
-                    Intent intent = null;
-                    intent = new Intent(Intent.ACTION_VIEW);
-                    intent.setData(Uri.parse(url));
-                    intent.putExtra(MxActionDefine.EXTRA_APPLICATION_ID,
-                            MxActionDefine.SOURCE_LOCAL_APPID);
-                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                    intent.setClassName(getContext(), BrowserActivity.class.getName());
-                    getContext().startActivity(intent);
-                    q.dimiss();
-                    
-                }
-            });
-            q.show(v);
+            showQuickDial(v);
             
         } else if (mHomeTab == v) {
             // gotoHomePage();
-            final QuickDialWindow q=new QuickDialWindow(mActivity);
-            q.getQuickDialDragLayer().setQuickDialOpenUrlListener(new QuickDialOpenUrlListener() {
-                
-                @Override
-                public void openUrl(String url) {
-                    Intent intent = null;
-                    intent = new Intent(Intent.ACTION_VIEW);
-                    intent.setData(Uri.parse(url));
-                    intent.putExtra(MxActionDefine.EXTRA_APPLICATION_ID,
-                            MxActionDefine.SOURCE_LOCAL_APPID);
-                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                    intent.setClassName(getContext(), BrowserActivity.class.getName());
-                    getContext().startActivity(intent);
-                    q.dimiss();
-                    
-                }
-            });
-            q.show(v);
-            
+           showQuickDial(v); 
         } else if (mMore == v) {
             showMenu();
         }
